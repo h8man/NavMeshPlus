adbccc81d7345b9e9d51d8c0621b85e2256dc5f4
vm <vmazurenko@bk.ru>
2019-02-26 01:06:17 +0200

    Fixed tile rotation

--- NavMeshComponents/Scripts/NavMeshBuilder2d.cs
+++ NavMeshComponents/Scripts/NavMeshBuilder2d.cs
@@ -104,7 +104,7 @@ namespace UnityEngine.AI
                     if (!builder.overrideByGrid && tilemap.GetColliderType(vec3int) == Tile.ColliderType.Sprite)
                     {
                         mesh = builder.GetMesh(tilemap.GetSprite(vec3int));
-                        src.transform = tilemap.GetTransformMatrix(vec3int) * Matrix4x4.Translate(tilemap.GetCellCenterWorld(vec3int));
+                        src.transform = Matrix4x4.Translate(tilemap.GetCellCenterWorld(vec3int)) * tilemap.GetTransformMatrix(vec3int);
                         src.sourceObject = mesh;
                         sources.Add(src);
                     }
